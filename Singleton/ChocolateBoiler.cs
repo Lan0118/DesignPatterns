6e5ee38406323435cef4a04d5e1c8ede7deefb7f
lsn <lsn@DESKTOP-B6J8QAG>
2019-11-07 08:58:49 +0800

    单例模式

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// 单例模式-巧克力工厂
+    ///创建唯一的锅炉对象，并提供全局访问点，懒加载。
+    ///多线程安全
+    /// </summary>
+    class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        private static ChocolateBoiler uniqueInstance;
+        private static readonly object obj = new object();
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+            Console.WriteLine("初始化: empty=" + empty + ". boiled="+ boiled);
+        }
+
+        public static  ChocolateBoiler getInstance()
+        {
+           
+            if (uniqueInstance == null)
+            {
+                lock (obj)
+                {
+                    {
+                        uniqueInstance = new ChocolateBoiler();
+                    }
+                }
+            }                               
+            return uniqueInstance;                     
+        }
+    }
+}
